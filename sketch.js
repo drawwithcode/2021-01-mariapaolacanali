c8646dbe2831fe1953b08f76e8befb1459e1de2c
mariapaolacanali <mariapaola.canali@mail.polimi.it>
2021-09-28 11:44:11 +0200

    updated

--- sketch.js
+++ sketch.js
@@ -1,13 +1,33 @@
-function preload() {
-  // put preload code here
-}
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
+  background(40);
+  angleMode(DEGREES);
+
+  frameRate(120);
 }
 
 function draw() {
-  // put drawing code here
-  background("red");
+  if (frameCount < 600) {
+    translate(width / 2, height / 2);
+    rotate(frameCount * 3);
+
+    noFill();
+
+    stroke(lerpColor(color("red"), color("blue"), frameCount / 750));
+
+    line(frameCount, 0, 0, frameCount);
+  } else if (frameCount >= 600 && frameCount < 610) {
+    fill(40);
+    rect(0, 0, windowWidth, windowHeight);
+  } else if (frameCount >= 610 && frameCount < 1100) {
+    loop();
+    translate(width / 2, height / 2);
+    rotate((frameCount - 600) * 400);
+
+    noFill();
+
+    stroke(lerpColor(color("blue"), color("red"), (frameCount - 600) / 750));
+
+    line(frameCount - 600, 0, 0, frameCount - 600);
+  }
 }
